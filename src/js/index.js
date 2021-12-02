50de3117aac1a8d272fe32d7eb16876c76179c4e
patrycjapetryk <patrycja.petryk@gmail.com>
2021-12-02 12:34:03 +0100

    Add mobile animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
 import '../scss/main.scss';
 
+// show images when loaded
+
 const images = document.querySelectorAll('.grid-gallery__image--js');
 
-images.forEach((item) => {
+const showImagesOnload = (item) => {
   item.style.opacity = 0;
 
   if (!item.complete) {
@@ -12,4 +14,49 @@ images.forEach((item) => {
   } else {
     item.style.opacity = 1;
   }
+};
+
+images.forEach((item) => {
+  showImagesOnload(item);
 });
+
+// packshots animation on mobile
+
+const animatedItems = document.querySelectorAll('.animation-on-mobile--js');
+
+let animationInterval;
+let opacity = 0;
+
+const changeItemsOpacity = () => {
+  if (opacity === 1) {
+    opacity = 0;
+  } else {
+    opacity = 1;
+  }
+  animatedItems.forEach((item) => {
+    item.style.opacity = opacity;
+  });
+};
+
+const clearAnimateOpacityInterval = () => {
+  if (animationInterval) {
+    clearInterval(animationInterval);
+  }
+};
+
+const setAnimateOpacityInterval = () => {
+  clearAnimateOpacityInterval();
+  animationInterval = setInterval(changeItemsOpacity, 2000);
+};
+
+const animateItemsOnMobile = () => {
+  if (window.innerWidth < 455) {
+    setAnimateOpacityInterval();
+  } else {
+    clearAnimateOpacityInterval();
+  }
+};
+
+animateItemsOnMobile();
+
+window.addEventListener('resize', animateItemsOnMobile);
